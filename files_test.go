424b027a9e9824e096e7008c8a0b60f1ea61d2df
Paweł Kopiczko <kopiczko@gmail.com>
2017-12-19 09:08:10 +0100

    test comments

--- files_test.go
+++ files_test.go
@@ -5,18 +5,19 @@ import "testing"
 func Test_NewFilesCluster(t *testing.T) {
 	files := NewFilesCluster(Cluster{})
 
+	// Make sure all paths in files are unique.
+
 	set := map[string]struct{}{}
 
 	for _, f := range files {
 		set[f.AbsolutePath] = struct{}{}
 	}
-
-	// Make sure all paths in files are unique.
 	if len(set) != len(files) {
 		t.Errorf("expected len = %d, got %d", len(set), len(files))
 	}
 
 	// Make sure all paths are absolute.
+
 	for _, f := range files {
 		if f.AbsolutePath[0] != '/' {
 			t.Errorf("expected to start with %q, got %q", "/", f.AbsolutePath)
