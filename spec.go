4400aa1104326d4bd772c73214f1b517e4af32e3
Ross Fairbanks <ross@giantswarm.io>
2018-03-22 14:48:22 +0100

    Godoc

--- spec.go
+++ spec.go
@@ -1,7 +1,13 @@
 package certs
 
 type Interface interface {
+	// SearchCluster searches for secrets containing TLS certs for guest
+	// clusters components.
 	SearchCluster(clusterID string) (Cluster, error)
+	// SearchDraining searches for secrets containing TLS certs for
+	// draining nodes in guest clusters.
 	SearchDraining(clusterID string) (Draining, error)
+	// SearchMonitoring searches for secrets containing TLS certs for
+	// monitoring guest clusters.
 	SearchMonitoring(clusterID string) (Monitoring, error)
 }
