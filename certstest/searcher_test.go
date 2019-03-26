ef12e78114374a154e8264615664255fd28949a7
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2019-03-26 15:13:36 +0100

    upgrade test to consume test case (#40)

--- certstest/searcher_test.go
+++ certstest/searcher_test.go
@@ -7,7 +7,8 @@ import (
 )
 
 func Test_CertsTest_NewSearcher(t *testing.T) {
-	s := NewSearcher()
+	config := Config{}
+	s := NewSearcher(config)
 	_, ok := interface{}(s).(certs.Interface)
 	if !ok {
 		t.Fatal("searcher does not implement correct interface")
